7d8c49e2aeab53340af4acf65bfe5f81fcbf80a0
iSar <i5ar@live.it>
2017-02-26 00:30:18 +0100

    Update to D3v4
    - Add JSON data;
    - Add range input.

--- main.js
+++ main.js
@@ -1,110 +1,164 @@
 var margin = { top: 50, right: 0, bottom: 100, left: 30 },
-          width = 960 - margin.left - margin.right,
-          height = 430 - margin.top - margin.bottom,
-          gridSize = Math.floor(width / 24),
-          legendElementWidth = gridSize*2,
-          buckets = 9,
-          colors = ["#ffffd9","#edf8b1","#c7e9b4","#7fcdbb","#41b6c4","#1d91c0","#225ea8","#253494","#081d58"], // alternatively colorbrewer.YlGnBu[9]
-          days = ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"],
-          times = ["1am", "2am", "3am", "4am", "5am", "6am", "7am", "8am", "9am", "10am", "11am", "12am", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm", "8pm", "9pm", "10pm", "11pm", "12pm"];
-          datasets = ["data.csv", "data2.csv"];
+    width = 960 - margin.left - margin.right,
+    height = 430 - margin.top - margin.bottom,
+    gridSize = Math.floor(width / 24),
+    legendElementWidth = gridSize*2,
+    buckets = 9,
+    colors = ["#ffffd9","#edf8b1","#c7e9b4","#7fcdbb","#41b6c4","#1d91c0","#225ea8","#253494","#081d58"],
+    days = ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"],
+    times = ["1am", "2am", "3am", "4am", "5am", "6am", "7am", "8am", "9am", "10am", "11am", "12am", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm", "8pm", "9pm", "10pm", "11pm", "12pm"];
+    // datasets = ["data.csv", "data2.csv"];
+    datasets = ["data.json", "data2.json"];
 
-      var svg = d3.select("#chart").append("svg")
-          .attr("width", width + margin.left + margin.right)
-          .attr("height", height + margin.top + margin.bottom)
-          .append("g")
-          .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+var svg = d3.select("#chart").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-      var dayLabels = svg.selectAll(".dayLabel")
-          .data(days)
-          .enter().append("text")
-            .text(function (d) { return d; })
-            .attr("x", 0)
-            .attr("y", function (d, i) { return i * gridSize; })
-            .style("text-anchor", "end")
-            .attr("transform", "translate(-6," + gridSize / 1.5 + ")")
-            .attr("class", function (d, i) { return ((i >= 0 && i <= 4) ? "dayLabel mono axis axis-workweek" : "dayLabel mono axis"); });
+var dayLabels = svg.selectAll(".dayLabel")
+    .data(days)
+    .enter().append("text")
+      .text(function (d) { return d; })
+      .attr("x", 0)
+      .attr("y", function (d, i) { return i * gridSize; })
+      .style("text-anchor", "end")
+      .attr("transform", "translate(-6," + gridSize / 1.5 + ")")
+      .attr("class", function (d, i) { return ((i >= 0 && i <= 4) ? "dayLabel mono axis axis-workweek" : "dayLabel mono axis"); });
 
-      var timeLabels = svg.selectAll(".timeLabel")
-          .data(times)
-          .enter().append("text")
-            .text(function(d) { return d; })
-            .attr("x", function(d, i) { return i * gridSize; })
-            .attr("y", 0)
-            .style("text-anchor", "middle")
-            .attr("transform", "translate(" + gridSize / 2 + ", -6)")
-            .attr("class", function(d, i) { return ((i >= 7 && i <= 16) ? "timeLabel mono axis axis-worktime" : "timeLabel mono axis"); });
+var timeLabels = svg.selectAll(".timeLabel")
+    .data(times)
+    .enter().append("text")
+      .text(function(d) { return d; })
+      .attr("x", function(d, i) { return i * gridSize; })
+      .attr("y", 0)
+      .style("text-anchor", "middle")
+      .attr("transform", "translate(" + gridSize / 2 + ", -6)")
+      .attr("class", function(d, i) { return ((i >= 7 && i <= 16) ? "timeLabel mono axis axis-worktime" : "timeLabel mono axis"); });
 
-      var heatmapChart = function(tsvFile) {
-        d3.csv(tsvFile,
-        function(d) {
-          return {
-            day: +d.day,
-            hour: +d.hour,
-            value: +d.value
-          };
-        },
-        function(error, data) {
-          var colorScale = d3.scale.quantile()
-              .domain([0, buckets - 1, d3.max(data, function (d) { return d.value; })])
-              .range(colors);
+var heatmapChart = function(csvFile) {
+  d3.json(
+    // url
+    csvFile,
+    // row
+    // function(data) {
+    //   return {
+    //     day: +data.day,
+    //     hour: +data.hour,
+    //     value: +data.value
+    //   };
+    // },
+    // callback
+    function(error, data) {
 
-          var cards = svg.selectAll(".hour")
-              .data(data, function(d) {return d.day+':'+d.hour;});
+      // NOTE: Transform JSON arrays to objects //////////////////////////
+      var arr=[];
+      for (i=0; i<data.length; i++) {
+        var obj = {"day": data[i][0], "hour": data[i][1], "value": data[i][2]};
+        arr.push(obj);
+      }
+      data = arr;
+      ////////////////////////////////////////////////////////////////////
 
-          cards.append("title");
+      var colorScale = d3.scaleQuantile()
+          .domain([0, buckets - 1, d3.max(data, function (d) { return d.value; })])
+          .range(colors);
 
-          cards.enter().append("rect")
-              .attr("x", function(d) { return (d.hour - 1) * gridSize; })
-              .attr("y", function(d) { return (d.day - 1) * gridSize; })
-              .attr("rx", 4)
-              .attr("ry", 4)
-              .attr("class", "hour bordered")
-              .attr("width", gridSize)
-              .attr("height", gridSize)
-              .style("fill", colors[0]);
+      var cards = svg.selectAll(".hour")
+          .data(data, function(d) {return d.day+':'+d.hour;});
 
-          cards.transition().duration(1000)
-              .style("fill", function(d) { return colorScale(d.value); });
+      cards.enter().append("rect")
+          .attr("x", function(d) { return (d.hour - 1) * gridSize; })
+          .attr("y", function(d) { return (d.day - 1) * gridSize; })
+          .attr("rx", 4)
+          .attr("ry", 4)
+          .attr("class", "hour bordered")
+          .attr("width", gridSize)
+          .attr("height", gridSize)
+          .style("fill", colors[0]);
 
-          cards.select("title").text(function(d) { return d.value; });
+      svg.selectAll(".hour")
+          .data(data, function(d) {return d.day+':'+d.hour;}).transition().duration(400)
+          .style("fill", function(d) { return colorScale(d.value); });
 
-          cards.exit().remove();
+      cards.exit().remove();
 
-          var legend = svg.selectAll(".legend")
-              .data([0].concat(colorScale.quantiles()), function(d) { return d; });
+      var legend = svg.selectAll(".legend")
+          .data([0].concat(colorScale.quantiles()), function(d) { return d; });
 
-          legend.enter().append("g")
-              .attr("class", "legend");
+      legend.enter().append("g")
+        .attr("class", "legend");
 
-          legend.append("rect")
-            .attr("x", function(d, i) { return legendElementWidth * i; })
-            .attr("y", height)
-            .attr("width", legendElementWidth)
-            .attr("height", gridSize / 2)
-            .style("fill", function(d, i) { return colors[i]; });
+      svg.selectAll(".legend")
+        .data([0].concat(colorScale.quantiles()), function(d) { return d; })
+        .append("rect")
+        .attr("x", function(d, i) { return legendElementWidth * i; })
+        .attr("y", height)
+        .attr("width", legendElementWidth)
+        .attr("height", gridSize / 2)
+        .style("fill", function(d, i) { return colors[i]; });
 
-          legend.append("text")
-            .attr("class", "mono")
-            .text(function(d) { return "≥ " + Math.round(d); })
-            .attr("x", function(d, i) { return legendElementWidth * i; })
-            .attr("y", height + gridSize);
+      svg.selectAll(".legend")
+        .data([0].concat(colorScale.quantiles()), function(d) { return d; })
+        .append("text")
+        .attr("class", "mono")
+        .text(function(d) { return "≥ " + Math.round(d); })
+        .attr("x", function(d, i) { return legendElementWidth * i; })
+        .attr("y", height + gridSize);
 
-          legend.exit().remove();
+      legend.exit().remove();
+    }
+  );
+};
 
-        });
-      };
+heatmapChart(datasets[0]);
 
-      heatmapChart(datasets[0]);
+var datasetpicker = d3.select("#dataset-picker").selectAll(".dataset-button")
+  .data(datasets);
 
-      var datasetpicker = d3.select("#dataset-picker").selectAll(".dataset-button")
-        .data(datasets);
+datasetpicker.enter()
+  .append("input")
+  .attr("value", function(d){ return "Dataset " + d })
+  .attr("type", "button")
+  .attr("class", "dataset-button")
+  .on("click", function(d) {
+    heatmapChart(d);
+  });
 
-      datasetpicker.enter()
-        .append("input")
-        .attr("value", function(d){ return "Dataset " + d })
-        .attr("type", "button")
-        .attr("class", "dataset-button")
-        .on("click", function(d) {
-          heatmapChart(d);
-        });
+// http://www.d3noob.org/2014/04/using-html-inputs-with-d3js.html
+////////////////////////////////////////////////////////////////////////////////
+var width = 600;
+var height = 300;
+
+var holder = d3.select("body")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+// Draw the circle
+holder.append("circle")
+  .attr("cx", 300)
+  .attr("cy", 150)
+  .style("fill", "none")
+  .style("stroke", "blue")
+  .attr("r", 120);
+
+// NOTE: When the input range changes update the circle
+d3.select("#nRadius").on("input", function() {
+  update(this.value);
+});
+
+// Initial starting radius of the circle
+update(120);
+
+// Update the elements
+function update(nRadius) {
+
+  // Adjust the text on the range slider
+  d3.select("#nRadius-value").text(nRadius);
+  d3.select("#nRadius").property("value", nRadius);
+
+  // Update the circle radius
+  holder.selectAll("circle")
+    .attr("r", nRadius);
+}
